0b815a1b46327cc309db915cd76fef6e913ef262
Anuj2508 <anuj.y@ecodedash.com>
2025-08-28 16:43:08 +0530

    Updated basename

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/">
+  <BrowserRouter basename="/blog/">
     <StrictMode>
       <App />
     </StrictMode>
