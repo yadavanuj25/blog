74bbc2fe2cd9deee467ef942665a0e0561db8834
Anuj2508 <anuj.y@ecodedash.com>
2025-08-28 15:30:36 +0530

    Added the basename

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 // src/App.jsx
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import BlogList from "./pages/BlogList";
 import BlogDetail from "./pages/BlogDetail";
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/blogs" element={<BlogList />} />
-        <Route path="/blogs/:id" element={<BlogDetail />} />
-      </Routes>
-    </Router>
+    // <Router>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/blogs" element={<BlogList />} />
+      <Route path="/blogs/:id" element={<BlogDetail />} />
+    </Routes>
+    // </Router>
   );
 };
 
